70ca7ca27675e807f0965744ca73033074b9f3e0
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-10-19 20:42:36 -0600

    Flask part 3

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask import render_template
+from flask import Flask, url_for
+from flask import request
 app = Flask(__name__)
+
+url_for('static', filename='style.css')
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 @app.route('/')
 def index():
@@ -18,13 +29,33 @@ def show_user_profile(username):
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
-    
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    error = None
     if request.method == 'POST':
-        do_the_login()
-    else:
-        show_the_login_form()
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+    
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
 
 if __name__ == '__main__':
     app.run()
