0530527e9fe700888dd21ddb472d062ff073223f
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-10-21 20:02:32 -0600

    Flask quick start part 4

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import Flask, url_for
 from flask import request
+from werkzeug import secure_filename
 app = Flask(__name__)
 
 url_for('static', filename='style.css')
@@ -14,7 +15,19 @@ def hello(name=None):
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    abort(401)
+    this_is_never_executed()
+
+@app.errorhandler(404)
+def not_found(error):
+    resp = make_response(render_template('error.html'), 404)
+    resp.headers['X-Something'] = 'A value'
+    return resp
+
 
 @app.route('/hello')
 def hello():
@@ -42,7 +55,7 @@ def login():
     # the code below is executed if the request method
     # was GET or the credentials were invalid
     return render_template('login.html', error=error)
-    
+
 @app.route('/projects/')
 def projects():
     return 'The project page'
@@ -56,6 +69,24 @@ with app.test_request_context('/hello', method='POST'):
     # end of the with block, such as basic assertions:
     assert request.path == '/hello'
     assert request.method == 'POST'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/' + secure_filename(f.filename))
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
 
 if __name__ == '__main__':
     app.run()
